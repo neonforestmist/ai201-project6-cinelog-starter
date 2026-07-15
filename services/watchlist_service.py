22bcfa4fcc65af733ddadd3c920fcd94c0f607cc
Lukas Lozada Perez <lukaslozada20@gmail.com>
2026-07-14 20:59:31 -0400

    feat: add watchlist visibility toggle

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -19,13 +19,14 @@ class NotInWatchlistError(Exception):
     pass
 
 
-def add_to_watchlist(user_id, film_id):
+def add_to_watchlist(user_id, film_id, public=True):
     """
     Add a film to a user's watchlist.
 
     Args:
         user_id (str): UUID of the user.
         film_id (str): UUID of the film.
+        public (bool): Whether the watchlist entry should be public.
 
     Returns:
         WatchlistEntry: The newly created entry.
@@ -46,7 +47,7 @@ def add_to_watchlist(user_id, film_id):
             f"Film '{film_id}' is already in this user's watchlist"
         )
 
-    entry = WatchlistEntry(user_id=user_id, film_id=film_id)
+    entry = WatchlistEntry(user_id=user_id, film_id=film_id, public=public)
     db.session.add(entry)
     db.session.commit()
     return entry
