8a5adbcd4dd5045d57e5f0dc00625e0efd76d58f
Lukas Lozada Perez <lukaslozada20@gmail.com>
2026-07-14 20:59:11 -0400

    test: cover watchlist removal behavior

--- tests/test_watchlist.py
+++ tests/test_watchlist.py
@@ -12,8 +12,10 @@ from models import User, Film, WatchlistEntry
 from services.collection_service import FilmNotFoundError
 from services.watchlist_service import (
     add_to_watchlist,
+    remove_from_watchlist,
     get_watchlist,
     AlreadyInWatchlistError,
+    NotInWatchlistError,
 )
 
 
@@ -93,6 +95,31 @@ def test_add_to_watchlist_nonexistent_film_raises(app, sample_user):
 
         with pytest.raises(FilmNotFoundError):
             add_to_watchlist(user_id=sample_user, film_id=fake_film_id)
+
+
+def test_remove_from_watchlist_deletes_entry(app, sample_user, sample_film):
+    """
+    Removing a watchlist film should delete the matching WatchlistEntry.
+    """
+    with app.app_context():
+        add_to_watchlist(user_id=sample_user, film_id=sample_film)
+
+        removed = remove_from_watchlist(user_id=sample_user, film_id=sample_film)
+
+        assert removed is True
+        in_db = WatchlistEntry.query.filter_by(
+            user_id=sample_user, film_id=sample_film
+        ).first()
+        assert in_db is None
+
+
+def test_remove_from_watchlist_missing_entry_raises(app, sample_user, sample_film):
+    """
+    Removing a film that is not on the watchlist should raise NotInWatchlistError.
+    """
+    with app.app_context():
+        with pytest.raises(NotInWatchlistError):
+            remove_from_watchlist(user_id=sample_user, film_id=sample_film)
 
 
 def test_get_watchlist_returns_newest_first(app, sample_user):
